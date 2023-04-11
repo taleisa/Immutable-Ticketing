ec14276f282cad4598ea11bbf804d84bf9c0219b
THE-REAL-JESUS <talal.3isa@gmail.com>
2023-04-11 08:37:14 +0300

    configure my tickets page

--- immutableTicketing/marketplace/urls.py
+++ immutableTicketing/marketplace/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import homePage, eventPage
+from .views import homePage, eventPage, ownedTickets,listTicket
 
 urlpatterns = [
     path("",homePage.as_view(),name = 'home'),
-    path("event/",eventPage.as_view(),name = 'events')
+    path("event/",eventPage.as_view(),name = 'events'),
+    path("myTickets/",ownedTickets.as_view(),name = 'myTickets'),
+    path("listTicket/<str:address>/",listTicket.as_view(),name = 'sellTicket'),
 ]
--- immutableTicketing/marketplace/views.py
+++ immutableTicketing/marketplace/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.views import View
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
 class homePage(LoginRequiredMixin, TemplateView):
     template_name = 'marketplace/marketplacePage.html'
@@ -10,3 +12,24 @@ class homePage(LoginRequiredMixin, TemplateView):
 class eventPage(LoginRequiredMixin, TemplateView):
     template_name = 'marketplace/eventPage.html'
     login_url = reverse_lazy("events")
+    
+class ownedTickets(LoginRequiredMixin,TemplateView):
+    template_name  = 'marketplace/myTicket.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        ticket1 = {'Title':'Kevin hart Stand up comedy','Time':'4:00PM','Location':'Boulevard','Date':'17/3/2026'}
+        ticket2 = {'Title':'Kevin hart Stand up comedy','Time':'4:00PM','Location':'Boulevard','Date':'17/3/2026'}
+        all_tickets = [ticket1,ticket2]
+        context["tickets"] = all_tickets
+        return context
+    
+    
+    
+class listTicket(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+        ticket_address = args['address']
+        ###################################################
+        ####Invoke transaction to list ticket on blockchain
+        ###################################################
+        return HttpResponseRedirect('myTickets')
