666b557d034a4ee132082d38b767227530850687
THE-REAL-JESUS <talal.3isa@gmail.com>
2023-04-11 06:59:49 +0300

    update marketplace template

--- immutableTicketing/marketplace/views.py
+++ immutableTicketing/marketplace/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 
 class homePage(LoginRequiredMixin, TemplateView):
-    template_name = 'marketplace/homePage.html'
+    template_name = 'marketplace/marketplacePage.html'
     login_url = reverse_lazy("login")
 
 class eventPage(LoginRequiredMixin, TemplateView):
