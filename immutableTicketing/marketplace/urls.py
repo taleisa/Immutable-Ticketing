d595e299dbf9fc5ee80c2758a66a6605029bca8b
THE-REAL-JESUS <talal.3isa@gmail.com>
2023-04-13 22:40:29 +0300

    configure my tickets page modify models to include is gea and eventhost

--- immutableTicketing/marketplace/urls.py
+++ immutableTicketing/marketplace/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import homePage, eventPage, ownedTickets,listTicket
+from .views import homePage, eventPage, ownedTickets, listTicket
 
 urlpatterns = [
-    path("",homePage.as_view(),name = 'home'),
-    path("event/",eventPage.as_view(),name = 'events'),
-    path("myTickets/",ownedTickets.as_view(),name = 'myTickets'),
-    path("listTicket/<str:address>/",listTicket.as_view(),name = 'sellTicket'),
+    path("", homePage.as_view(), name="home"),
+    path("event/", eventPage.as_view(), name="events"),
+    path("my-tickets/", ownedTickets.as_view(), name="myTickets"),
+    path("list-ticket/<str:address>/", listTicket.as_view(), name="sellTicket"),
 ]
