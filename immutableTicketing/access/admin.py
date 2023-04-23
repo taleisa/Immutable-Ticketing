3484ffdbb7ef8956cb2f9938673f259d755c9ed7
THE-REAL-JESUS <talal.3isa@gmail.com>
2023-04-23 09:03:58 +0300

    use ticket functionality

--- immutableTicketing/access/admin.py
+++ immutableTicketing/access/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from access.models import web3User
-from .models import Event
-# Define an inline admin descriptor for Employee model
-# which acts a bit like a singleton
+from access.models import web3User, Event, Gate
+
 class web3UserInline(admin.StackedInline):
     model = web3User
     can_delete = False
@@ -20,3 +18,4 @@ admin.site.register(User, UserAdmin)
 
 
 admin.site.register(Event)
+admin.site.register(Gate)
